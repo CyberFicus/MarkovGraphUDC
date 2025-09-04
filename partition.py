574997eab2451fc33522c8588416e2bd8ff9083d
CyberFicus <cyberficus9997@gmail.com>
2025-09-04 12:52:22 +0300

    partition fix
    sets must have LAMBDA by default
    
    also pref, root and suff properties now return LAMBDA constant instead of empty string

--- partition.py
+++ partition.py
@@ -1,8 +1,8 @@
 from config import LAMBDA
 
 class Partition:
-    suffixes = set()
-    prefixes = set()
+    suffixes = set(LAMBDA)
+    prefixes = set(LAMBDA)
 
     def __init__(self, codeword: str, root_idx: int, suff_idx: int):
         if (root_idx < 0 or root_idx > suff_idx or suff_idx > len(codeword)):
@@ -15,41 +15,23 @@ class Partition:
         Partition.suffixes.add(self.suff)
 
     def __get_pref(self ):
-        return self.word[:self.root_idx]
+        res = self.word[:self.root_idx]
+        return LAMBDA if res == "" else res 
     
     def __get_root(self):
-        return self.word[self.root_idx:self.suff_idx]
+        res = self.word[self.root_idx:self.suff_idx]
+        return LAMBDA if res == "" else res
 
     def __get_suff(self):
-        return self.word[self.suff_idx:]    
+        res = self.word[self.suff_idx:]
+        return LAMBDA if res == "" else res    
 
     pref = property(__get_pref)
     root = property(__get_root)
     suff = property(__get_suff)
 
     def __str__(self):
-        res = ""
-        
-        if self.root_idx == 0:
-            res += LAMBDA
-        else:
-            res += self.pref
-
-        res += "."
-
-        if self.root_idx == self.suff_idx:
-            res += LAMBDA
-        else:
-            res += self.root
-
-        res += "."
-
-        if (self.suff_idx == len(self.word)):
-            res += LAMBDA
-        else:
-            res += self.suff
-        
-        return res
+        return f"{self.pref}.{self.root}.{self.suff}"
 
     def __repr__(self):
         return Partition.__str__(self)
@@ -65,6 +47,3 @@ class Partition:
             self.root_idx == value.root_idx and
             self.suff_idx == value.suff_idx
         )
-
-
-
