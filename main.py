564f694948e1819fe05fcdda94debf9999b59090
CyberFicus <cyberficus9997@gmail.com>
2025-09-04 15:37:36 +0300

    Finding cycle in graph

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from config import LAMBDA
 from utils import parse_file
 from partition import Partition as prt
+from graph import Node as node, Link as link 
 
 def findall(base_word: str, root_word: str):
     res = []
@@ -85,16 +87,113 @@ def get_all_partitions(codewords: list):
                     res.add(partition)
     return res
 
+def build_graph(partitions: list):
+    node_names = prt.prefixes.intersection(prt.suffixes)
+    lambda_node = None
 
+    # create nodes
+    for node_name in node_names:
+        if (not isinstance(node_name, str)):
+            continue
+
+        n = node(node_name)
+        if (node_name == LAMBDA):
+            lambda_node = n
+
+    # build links
+    for partition in partitions:
+        if (
+            not isinstance(partition, prt) or
+            not partition.pref in prt.suffixes or
+            not partition.suff in prt.prefixes
+        ):
+            continue
+        if (partition.pref not in node.nodes):
+            print(f"WARNING: {partition.pref} not in nodes!")
+            continue
+        if (partition.suff not in node.nodes):
+            print(f"WARNING: {partition.suff} not in nodes!")
+            continue
+
+        src = node.nodes[partition.pref]
+        dst = node.nodes[partition.suff]
+
+        if (not isinstance(src, node)):
+            print(f"WARNING: src {partition.pref} is not a node!")
+        if (not isinstance(dst, node)):
+            print(f"WARNING: dst {partition.suff} is not a node!")
+
+        src.link(dst, partition.root)
+
+    if (lambda_node is None):
+        print("WARNING: lambda node is none!!!")
+    
+    return lambda_node
+
+passed_nodes = set()
+
+def find_way(src: node, dst :node):
+    res = []
+    passed_nodes.add(src)
+
+    def find_link_to(dst :node):
+        if (dst not in src.linked_nodes):
+            raise ValueError(f"{dst} not linked to {src}")
+        
+        for l in src.links:
+            if (not isinstance(l, link)):
+                print(f"WARNING: {l} in links of node {src}")
+                continue
+            if (l.src != src):
+                print(f"WARNING: link {l} does not belong to node {src}")
+                continue
+
+            if (l.dst == dst):
+                return l
+        
+        raise ValueError(f"{src} linked to {dst} with no link")
+
+    if ( # dst one link away
+        src != dst and        
+        dst in src.linked_nodes
+    ):     
+        passed_nodes.remove(src)
+        return [find_link_to(dst)]
+
+    for n in src.linked_nodes:
+        if (not isinstance(n, node)):
+            print(f"WARNING: {n} in linked nodes of {src}")
+            continue
+        if ( # infinite loop ahead
+            n == src or 
+            n in passed_nodes
+        ): 
+            continue
+
+        buf = find_way(n, dst)
+        if (buf != []):
+            res = [find_link_to(n)]
+            res.extend(buf)
+            passed_nodes.remove(src)   
+            return res
+    
+    passed_nodes.remove(src)
+    return res
+        
 fname = "input/in1.txt"
 codewords = parse_file(fname)
 
 partitions = get_all_partitions(codewords)
-final_partitions = set([p for p in partitions if (
-    isinstance(p, prt) and
-    p.pref in prt.suffixes and
-    p.suff in prt.prefixes
-)])
 
 print("\nPartitions:")
-print(final_partitions)
+print(partitions)
+
+lambda_node = build_graph(partitions)
+cycle = find_way(lambda_node, lambda_node)
+
+if (cycle != []):
+    print("\nCycle is:")
+    for l in cycle:
+        print(l)
+else:
+    print("\nCycle not found")
